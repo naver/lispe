637ebecc816ced97b868b7e31b8d5822c13ce8f4
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2020-12-09 09:22:42 +0100

    Update fibo.lisp

--- examples/diverse/fibo.lisp
+++ examples/diverse/fibo.lisp
@@ -1,17 +1,24 @@
+; fibonacci with memoization
+; note that: (key d n v) returns d...
+
 (defun fibo (n (d {1:1}) )
    (if (at d n)
       (at d n)
-      (ncheck (> n 1)
+      (ife (<= n 1)
          1
-         (key d n (+ (fibo (- n 1) d) (fibo (- n 2) d)))
-         (key d n)
+         (key 
+            (key d n 
+               (+ 
+                  (fibo (- n 1) d) 
+                  (fibo (- n 2) d)
+               )
+            ) 
+            n
+         )
       )
    )
 )
 
-(println (fibo 10))
-
-
-
+(println (fibo 20))
 
 
