4c4ddef5c8617d18cb90f86e6057e6970f43ecfe
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2020-12-09 09:58:52 +0100

    Update fibo.lisp

--- examples/diverse/fibo.lisp
+++ examples/diverse/fibo.lisp
@@ -2,17 +2,17 @@
 ; note that: (key d n v) returns d...
 
 (defun fibo (n (d {1:1}) )
-   (if (at d n)
+   (select 
       (at d n)
-      (ife (<= n 1)
+      (if (<= n 1)
          1
-         (key 
-            (key d n 
-               (+ 
-                  (fibo (- n 1) d) 
+         (key
+            (key d n
+               (+
+                  (fibo (- n 1) d)
                   (fibo (- n 2) d)
                )
-            ) 
+            )
             n
          )
       )
@@ -20,5 +20,6 @@
 )
 
 (println (fibo 20))
+
 
 
