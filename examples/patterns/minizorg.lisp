10b1a337a3200c20df5de94e9df36d470da3338e
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2021-02-01 16:19:05 +0100

    Update minizorg.lisp

--- examples/patterns/minizorg.lisp
+++ examples/patterns/minizorg.lisp
@@ -3,123 +3,78 @@
 ; Actions on data structures
 (defmacro belong (x l) (in l (keystr x)))
 
-(data (Move _) (Break _ _) (Open _ _) (Kill _ _) (Pick _ _) (Take _) (Drop _))
+(data [Move _] [Break _ _] [Open _ _] [Kill _ _] [Pick _ _] [Take _] [Drop _])
 
 ; pick up is the same as take with one more parameter
-(defpat action ( (Pick 'up x))
+(defpat action ( [Pick 'up x] )
    (action (Take x))
 )
 
 ; take an object on the ground
-(defpat action ( (Take x) )
-   (if
-      (check_object position x)
-      (block
-         (push belongings x)
-         (println "Ok we have picked up" x)
-      )
+(defpat action ( [Take x] )
+   (ncheck (check_object position x)
       (println "Cannot pick up the" x)
+      (push belongings x)
+      (println "Ok we have picked up" x)
    )
 )
 
 ; drop it at current position
-(defpat action ( (Drop x))
-   (if
-      (check_belongings x)
-      (block
-         (key objects (keystr position) x)
-         (pop belongings (find belongings x))
-         (println "Ok! We have dropped" x " on the ground")
-         (display_position position)
-      )
+(defpat action ( [Drop x] )
+   (check (check_belongings x)
+      (key objects (keystr position) x)
+      (pop belongings (find belongings x))
+      (println "Ok! We have dropped" x " on the ground")
+      (display_position position)
    )
 )
 
 ; break a window with an object: it has to be a stone
-(defpat action( (Break 'window x))
-   (if (= position '(1 2))
-      (if (and (check_belongings x) (eq x 'stone))
-         (block
-            (update_direction (keystr position) "0:2")
-            (key castlemap "1:2" "You are standing in front of a broken window")
-            (println "Window is broken")
-         )
-         (println "Cannot break a window with that" x)
-      )
+(defpat action( [Break 'window x] )
+   (ncheck (= position '(1 2))
       (println "Get a sight check. There is no window over here")
+      (ncheck (and (check_belongings x) (eq x 'stone))
+         (println "Cannot break a window with that" x)
+         (update_direction (keystr position) "0:2")
+         (key castlemap "1:2" "You are standing in front of a broken window")
+         (println "Window is broken")
+      )
    )
 )
 
 ; open door with a key
 ; It triggers new potential move to other positions
-(defpat action ((Open 'door x))
-   (if (= position '(1 1))
-      (if (and (check_belongings x) (eq x 'key))
-         (block
-            (update_direction (keystr position) "0:1")
-            (update_direction "0:1" "0:0")
-            (key castlemap "1:1" "You are standing in front of an open gate")
-            (println "The door opens")
-         )
-         (println "Cannot open a door with that" x)
-      )
+(defpat action ( [Open 'door 'key] )
+   (ncheck (= position '(1 1))
       (println "A door here??? Really!!!")
+      (ncheck (check_belongings 'key)
+         (println "You do not own a key")
+         (update_direction (keystr position) "0:1")
+         (update_direction "0:1" "0:0")
+         (key castlemap "1:1" "You are standing in front of an open gate")
+         (println "The door opens")
+      )
    )
 )
 
 ; kill the ogre with a sword or you'll get into trouble
-(defpat action ((Kill 'ogre x))
-   (if (= position '(1 0))
-      (if (and (check_belongings x) (eq x 'sword))
-         (block
-            (setg theend true)
-            (println "You kill the ogre. He was guarding an wonderfull treasure. You are now rich beyond measure. Congratulation!!!")
-         )
+(defpat action ( [Kill 'ogre x] )
+   (ncheck (= position '(1 0))
+      (println "There is no ogre here")
+      (ncheck (and (check_belongings x) (eq x 'sword))
          (block
             (push dangers "1:0")
             (println "This is not a very efficient weapon. The ogre attacks you...")
          )
+         (setg theend true)
+         (println "You kill the ogre. He was guarding an wonderfull treasure. You are now rich beyond measure. Congratulation!!!")
       )
-      (println "There is no ogre here")
    )
 )
 
 ; Moving from one position to another
-(defpat action ( (Move 'north))
-   (setq p (checkposition (- (car position) 1) (cadr position)))
-   (if (= p position)
-      (println "Cannot go in this direction")
-      (block
-         (setg position p)
-         (display_position position)
-      )
-   )
-)
-
-(defpat action ( (Move 'south))
-   (setg p (checkposition (+ (car position) 1) (cadr position)))
-   (if (= p position)
-      (println "Cannot go in this direction")
-      (block
-         (setg position p)
-         (display_position position)
-      )
-   )
-)
-
-(defpat action ( (Move 'west))
-   (setg p (checkposition (car position) (- (cadr position) 1)))
-   (if (= p position)
-      (println "Cannot go in this direction")
-      (block
-         (setg position p)
-         (display_position position)
-      )
-   )
-)
-
-(defpat action ( (Move 'east))
-   (setg p (checkposition (car position) (+ (cadr position) 1)))
+(defpat action ( [Move direction] )
+   (setq p (checkposition (zipwith '+ position (key directions direction))))
    (if (= p position)
       (println "Cannot go in this direction")
       (block
@@ -132,6 +87,7 @@
 ; Default action
 (defpat action(_) (random_choice 1 msgs 10))
 
+; Primitives
 ; Data for the game and basic instructions
 ; build the key string
 (defun keystr(p)
@@ -143,6 +99,20 @@
    (if (belong p (key moving (keystr position)))
       p
       position
+   )
+)
+
+
+; check if we are within the confines of the game: 3x3 square map.
+(defpat checkposition( [x y] )
+   (check_valid_path
+      (cond
+         ((< x 0) (list 0 y))
+         ((< y 0) (list x 0))
+         ((> x 2) (list 2 y))
+         ((> y 2) (list x 2))
+         (true (list x y))
+      )
    )
 )
 
@@ -158,20 +128,6 @@
       )
    )
 )
-
-; check if we are within the confines of the game: 3x3 square map.
-(defun checkposition(x y)
-   (check_valid_path
-      (cond
-         ((< x 0) (list 0 y))
-         ((< y 0) (list x 0))
-         ((> x 2) (list 2 y))
-         ((> y 2) (list x 2))
-         (true (list x y))
-      )
-   )
-)
-
 
 ; check if the object is available on the ground and pick it up
 ; we also remove it from objects
@@ -206,6 +162,9 @@
 (defun check_danger (position)
    (belong position dangers)
 )
+
+; each direction is associated with some values to add position with
+(setq directions { "north": '(-1 0) "south":'(1 0) "west":'(0 -1) "east":'(0 1)})
 
 ; some synonyms
 (setq synonyms
@@ -267,16 +226,6 @@
    }
 )
 
-; initialisation
-(setq belongings '())
-(setq position '(1 1))
-(setq dangers '("2:1"))
-(setq theend nil)
-(setq commands '(commencement))
-
-; We display our initial psoition
-(display_position position)
-
 (setq msgs '(
       "Not sure what you want to do!!!"
       "Do not know what to do here"
@@ -287,6 +236,16 @@
       "Nice try... But no"
    )
 )
+
+; initialisation
+(setq belongings '())
+(setq position '(1 1))
+(setq dangers '("2:1"))
+(setq theend nil)
+(setq commands '(commencement))
+
+; We display our initial psoition
+(display_position position)
 
 (while (neq (car commands) 'End)
    (print "Your order sire? ")
@@ -300,36 +259,28 @@
 
    (setq dialog (lower dialog))
    (setq dialog (+ (upper (at dialog 0)) (extract dialog 1 (size dialog))))
-   (setq commands (split dialog " "))
-   (setq commands (map (\ (x) (select (key synonyms x) x)) commands))
+   (setq commands (map (\ (x) (select (key synonyms x) x)) (split dialog " ")))
    ; we transform each of our strings into atoms, for match sake
    ;we remove useless words: the, a etc..
    (setq commands (filter (\ (x) (not (key stopwords x))) (map 'atom commands)))
+   
    ; our commands is now a list of atoms that should match a data structure
+   ; if we cannot evaluate it, then we display a random error message   
    (maybe
       (println (action commands))
       (println (random_choice 1 msgs 10))
    )
-   (if (check_danger position)
-      (block
-         (println "you are dead!!!")
-         (setq commands '(End))
-      )
+      
+   (check (check_danger position)
+      (println "you are dead!!!")
+      (setq commands '(End))
    )
 
-   (if (eq theend true)
+   (if theend
       (setq commands '(End))
    )
 )
 
 (print "The end")
-
-
-
-
-
-
-
-
 
 
