4dfb530b72fea00e1e626728d67d75fa0b046728
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2022-10-20 11:04:39 +0200

    Update apply_rules.lisp

--- examples/patterns/apply_rules.lisp
+++ examples/patterns/apply_rules.lisp
@@ -3,13 +3,9 @@
 ;Description: Apply rules
 
 
-(defmacro islexicalrule (POS)
-   (eq "%" (@ POS 0))
-)
-
-
 ; Generation from a POS
-; We go down our list to find a terminal POS (starting with a "%")
+; either it is a rule or it its lexical rule
+; lexical rules are not LIST
 (defpat generate([POS $ current_pos] tree)
    (setq r (key grammar POS))
    (if (consp r)
@@ -34,27 +30,18 @@
    (generate current_pos consume)
 )
 
-; If the first element in current_pos is a terminal element
-; then we check if the word exists in the grammar
-(defpat match ( [ (islexicalrule POS) $ current_pos]  [w $ sentence]  consume)
-   ;If the word exists in the grammar
-   ;We continue to analyse our sequence of POS
-   ;Else we simply stop
-   (if (in (key grammar POS) w)
-      (match current_pos sentence  (nconcn consume w))
-   )
-)
-
 ; We check the first POS from current_pos
 ; We check if it is a lexicon rule (r is not a list then)
 ; Otherwise, we take the rule and replace the current POS
 ; with its rule description.
-(defpat match ( [POS $ current_pos] s consume)
+(defpat match ( [POS $ current_pos] [w $ sentence] consume)
    (setq R (key grammar POS))
    (ncheck (consp R)
-      (match (nconcn (list R) current_pos) s consume)
+      (if (in (key grammar R) w)
+         (match current_pos sentence (nconcn consume w))
+      )
       (loop r R
-         (setq  poslst (match (nconcn r current_pos) s consume))
+         (setq  poslst (match (nconcn r current_pos) (cons w sentence) consume))
          (if poslst
             (return poslst)
          )
@@ -72,7 +59,4 @@
    (setq sentence (parse (tokenize sentence) "S" ()))
    (@@ sentence 0 -1)
 )
-
-
-
 
