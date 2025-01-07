7d3f8472c7038832b16b9c1ef902240884b6c700
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2025-01-07 13:40:30 +0100

    Update day9.lisp

--- examples/AdventOfCode2024/day9.lisp
+++ examples/AdventOfCode2024/day9.lisp
@@ -3,19 +3,22 @@
 ;Description: Advent of Code 2024 day 9
 
 
-(setq codes (integers (split (trim (fread (+ _current "data/day9_example.txt"))) "")))
+(setq codes (integers (split (trim (fread (+ _current "data/day9.txt"))) "")))
 
 (setq base ())
 
 (setq blocs (slice codes 2))
 
+(setq lst (last blocs))
+(pop blocs)
+(push blocs (list (car lst) 0))
+
 (loop b (enum blocs)
    (push base (replicate (@ b 1 0) (@ b 0)))
-   (maybe
-      (push base (replicate (@ b 1 1) "."))
-      (setq déplie (flatten base))
-   )
+   (push base (replicate (@ b 1 1) "."))
 )
+
+(setq déplie (flatten base))
 
 (setq nv (find déplie "."))
 (setq dernier (- (size déplie) 1))
@@ -35,52 +38,49 @@
 
 (println somme)
 
-(defun cherche (b nv)
-   (while 
-      (and 
-         (< nv (size b)) 
-         (nullp (in (@ b nv) "."))
-      )
-      (+= nv 1)
-   )
+(setq i 0)
+(loop b blocs
+   (push (@ blocs i) (replicate (@ b 0) i))
+   (+= i 1)
 )
 
-(defmacro à(i) (size (@ base i)))
-
-(setq base (filterlist '(neq _ ()) base))
-(println base)
-(setq sz (size base))
-(setq nv -1)
-
-(setq rb (reverse (clone base) true))
-(loop dder (enum rb)
-   (setq der (@ dder 1))
-   (println 0 dder base)
-   (loop dd (enum base)
-      (setq d (@ dd 1))
+(setq sz (size blocs))
+(loop i (irange (- sz 1) -1 -1)
+   (setq val (@ blocs i 2))
+   (setq szval (size val))
+   (loop b (enum blocs)
       (check 
-         (and 
-            (in d ".")
-            (<= (size der) (size d))
+         (and
+            (< (@ b 0) i)
+            (>= (@ b 1 1) szval)
          )
-         (pop base (- sz (@ dder 0)))
-         (ncheck (!= (size der) (size d))
-            (set@ base (@ dd 0) der)
-            (set@ base (@ dd 0) (@@ d 0 (- (size d) (size der))))
-            (insert base der (@ dd 0))
+         (-= (@ blocs (@ b 0) 1) szval)
+         (push (@ blocs (@ b 0)) val)
+         (set@ blocs i 2 ())
+         (if (> (size (@ blocs i)) 2)
+            (+= (@ blocs (- i 1) 1) szval)
+            (+= (@ blocs i 1) szval)
          )
-         (setq bb (list (car base)))
-         (setq base bb)
-         (println 1 der base)
          (break)
       )
    )
-   (println 2 der base)
+)
+
+; 00...111...2...333.44.5555.6666.777.888899
+; 00992111777.44.333....5555.6666.....8888..
+
+(defun fusionne(blocs)
+   (setq l ())
+   (loop b blocs
+      (nconc l (cddr b))
+      (nconc l (replicate (@ b 1) "."))
+   )
+   (flatten l)
 )
 
 
-
-
-
-
+(setq somme 0)
+(loop c (enum (fusionne blocs))
+   (+= somme (* (@ c 0) (@ c 1)))
+)
 
